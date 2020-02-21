2d08c02db6bd3426a97511f5ccafbbb8f417975a
Woodward <DavidWoodward@my.unt.edu>
2020-02-20 20:31:44 -0600

    push

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,8 +13,8 @@ const s3 = new AWS.S3({
     //accessKeyId: process.env.AWS_ACCESS_KEY, 
     //secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     // Remove keys on git push
-    accessKeyId: 'AKIAIT7ZONAGZPSHLFOA',
-    secretAccessKey: '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',
+    accessKeyId: '',
+    secretAccessKey: '',
     region: 'us-east-2'
 });
 
